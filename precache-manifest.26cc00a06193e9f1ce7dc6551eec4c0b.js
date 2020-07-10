b1003732bcc9e91145825b3ebab0df154f046f30
Lilit <lilit@inone.am>
2020-07-11 02:50:20 +0400

    Updates

---
+++ precache-manifest.26cc00a06193e9f1ce7dc6551eec4c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e332ebd4df6d83f7ff40578da9513dd",
+    "url": "/news_app/index.html"
+  },
+  {
+    "revision": "1f93825c2b61a86bfd8a",
+    "url": "/news_app/static/css/main.611e75f6.chunk.css"
+  },
+  {
+    "revision": "4dc5c0ca39c1bc547b4b",
+    "url": "/news_app/static/js/2.55882f22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news_app/static/js/2.55882f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f93825c2b61a86bfd8a",
+    "url": "/news_app/static/js/main.13503972.chunk.js"
+  },
+  {
+    "revision": "ee3cb7f0b86e67490bef",
+    "url": "/news_app/static/js/runtime-main.a4a62132.js"
+  }
+]);
